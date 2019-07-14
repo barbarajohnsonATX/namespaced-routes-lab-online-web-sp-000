3ebf645b9067360559eea89323755c9c8276e0c3
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-14 20:55:04 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,15 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+   # @artist = Artist.new
+   #redirect when access is turned off
+    @preference = Preference.first
+    if @preference.allow_create_artists
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
+    
   end
 
   def create
