e96d90b834ed0aeccf5217cd0f259afa95edb9a2
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-14 20:41:50 +0000

    Automatically backed up by Learn

--- db/migrate/20190714042232_create_preferences_table.rb
+++ db/migrate/20190714042232_create_preferences_table.rb
@@ -1,4 +1,4 @@
-class CreatePreferences < ActiveRecord::Migration[5.0]
+class CreatePreferencesTable < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
       t.string :song_sort_order
