c59bfdc08f70f3851dc1a299f0f6f0457d4dfd97
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-14 21:03:48 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,17 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all
+    #@artists = Artist.all
+    
+    if @preference
+      if @preference.artist_sort_order = "ASC"
+        @artists = Artist.all.sort_by{|artist| artist.name}
+      else
+        @artists = Artist.all.sort_by{|artist| artist.name}.reverse
+      end
+    else
+      @artists = Artist.all
+    end
+    
   end
 
   def show
